62124820f2339a6d8e9cf8ec57c77fff791a1e87
Ryan <109833217+Ryany0@users.noreply.github.com>
2025-07-23 01:41:27 -0700

    Add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+require("dotenv").config();
 
 //const newMessageRouter = require("./controller/new");
 const  { Router } = require("express");
